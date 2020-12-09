9d98fa8a40217757f499d7a434f0f876ec41ae34
Bartok <thom.huenger@gmail.com>
2020-12-09 18:14:17 -0500

    finished JS

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
-var generateBtn = document.querySelector("#generate");
-var pwdLength = prompt("How long do you want your password to be? 8-128 characters");
-var lowerCase = confirm("Do you want to include lowercase letters in your password?");
-var upperCase = confirm("Do you want your password to include uppercase letters?");
-var num = confirm("Do you want your password to include numbers?");
-var special = confirm("Do you want your password to include special characters?");
-
-
-
-
-function getLowerCase() {
-
-}
-function getUpperCase(){
-
-}
-
-function getNumber() {
-
-}
-
-function getSpecial() {
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "="];
+  var charChoices = []
   
-}
+  var generateBtn = document.querySelector("#generate") 
+  function generatePassword() {
+  
+ var  pwdLength = prompt("How long do you want your password to be? 8-128 characters");
+ if (isNaN(pwdLength)) { 
+   alert("You must choose a number, dummy!")
+   return
+ }
 
+//if pwd is valid...
+
+ var includeLowerCase = confirm("Do you want to include lowercase letters in your password?");
+ var includeUpperCase = confirm("Do you want your password to include uppercase letters?");
+ var includeNumber = confirm("Do you want your password to include numbers?");
+ var includeSpecial = confirm("Do you want your password to include special characters?");
+
+
+ 
+ if (includeLowerCase) {
+   charChoices = charChoices.concat(lowerCase);
+ }
+ 
+ if (includeUpperCase) {
+   charChoices = charChoices.concat(upperCase);
+  }
+ 
+ if (includeNumber) {
+   charChoices = charChoices.concat(numbers);
+ }
+
+ if (includeSpecial) {
+   charChoices = charChoices.concat(specialChar);
+ }
+var password = "" 
+
+for (var i = 0; i < pwdLength; i++) {
+  var randomNum = Math.floor(Math.random() * charChoices.length)
+  password += charChoices[randomNum];
+}
+return password;
+};
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
 
   passwordText.value = password;
 
